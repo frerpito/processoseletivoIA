c1864b5b86f680657f055c9c20e5c5606f5a0fce
frerpito <franchicobleh@gmail.com>
2026-04-27 11:07:46 -0300

    refactor: reorganizar código de conversão para TFLite e melhorar a legibilidade

--- optimize_model.py
+++ optimize_model.py
@@ -1,19 +1,28 @@
-from tensorflow import keras
 import tensorflow as tf
-import os
 
-#insira seu código aqui
-model = keras.models.load_model("model.h5")
+# ====================================
+# 1. Carregamento do  modelo treinado
+# ====================================
+model = tf.keras.models.load_model("model.h5")
 
-# converter para tflite
+
+
+# =========================
+# 2. Conversão para TFLite
+# =========================
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Ativa quantização (Dynamic Range Quantization)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
-tflite_dynamic = converter.convert()
+tflite_model = converter.convert()
 
-# salvar na raiz do projeto
-tfl_dyn_path = "model.tflite"
 
-with open(tfl_dyn_path, "wb") as f:
-    f.write(tflite_dynamic)
 
+# ===============================
+# 3. Salvando o modelo otimizado
+# ===============================
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
+
+print("Modelo convertido e salvo como model.tflite")
